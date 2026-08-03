7d38f15c63232daedc713784479d323fdc81016e
EBuravskiy <elric0sphinx@gmail.com>
2026-08-03 13:34:50 +0500

    An interface and a class implementing it for database operations have been added.

---
+++ BookStore.DataAccess/Repositories/BookRepository.cs
@@ -0,0 +1,69 @@
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DataAccess.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookStoreDbContext _dbContext;
+
+        public BookRepository(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<Book>> GetAll()
+        {
+            List<BookEntity> bookEntities = await _dbContext.Books
+                .AsNoTracking()
+                .ToListAsync();
+            List<Book> books = bookEntities
+                .Select(b => Book.Create(b.BookID, b.BookTitle, b.BookDescription, b.BookPrice).Book)
+                .ToList();
+            return books;
+        }
+        //Асинхронный метод получения элемента Book из базы данных
+        public async Task<Book> Get(Guid bookID)
+        {
+            BookEntity bookEntity = await _dbContext.Books.FirstOrDefaultAsync(b => b.BookID == bookID);
+            Book book = null;
+            if (bookEntity != null)
+            {
+                book = Book.Create(bookEntity.BookID, bookEntity.BookTitle, bookEntity.BookDescription, bookEntity.BookPrice).Book;
+            }
+            return book;
+        }
+        public async Task<Guid> Create(Book book)
+        {
+            BookEntity bookEntity = new BookEntity()
+            {
+                BookID = book.BookID,
+                BookTitle = book.Title,
+                BookDescription = book.Description,
+                BookPrice = book.Price,
+            };
+            await _dbContext.Books.AddAsync(bookEntity);
+            await _dbContext.SaveChangesAsync();
+            return bookEntity.BookID;
+        }
+        //Асинхронный метод обновления данных книги в базе данных
+        public async Task<Guid> Update(Guid bookID, string title, string description, decimal price)
+        {
+            await _dbContext.Books
+                .Where(b => b.BookID == bookID)
+                .ExecuteUpdateAsync(s => s
+                .SetProperty(b => b.BookTitle, b => title)
+                .SetProperty(b => b.BookDescription, b => description)
+                .SetProperty(b => b.BookPrice, b => price));
+            return bookID;
+        }
+        //Асинхронный метод удаления книги из базы данных
+        public async Task<Guid> Delete(Guid bookID)
+        {
+            await _dbContext.Books
+                .Where(b => b.BookID == bookID)
+                .ExecuteDeleteAsync();
+            return bookID;
+        }
+    }
+}
---
+++ BookStore.DataAccess/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Core.Models;
+
+namespace BookStore.DataAccess.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<Guid> Create(Book book);
+        Task<Guid> Delete(Guid bookID);
+        Task<Book> Get(Guid bookID);
+        Task<List<Book>> GetAll();
+        Task<Guid> Update(Guid bookID, string title, string description, decimal price);
+    }
+}
