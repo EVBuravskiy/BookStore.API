63865ddfcf61db81ad391b8fe60fdf98ca26af6b
EBuravskiy <elric0sphinx@gmail.com>
2026-08-03 11:00:13 +0500

    Добавьте файлы проекта.

---
+++ BookStore.Core/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Core.Models
+{
+    public class Book
+    {
+        public Guid BookID { get; }
+        public string Title { get; } = string.Empty;
+        public string Description { get; } = string.Empty;
+        public decimal Price { get; }
+    }
+}
