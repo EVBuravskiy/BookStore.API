d39b7aee140245f06d2a5b97ab311fa1dcfc7fed
EBuravskiy <elric0sphinx@gmail.com>
2026-08-03 13:03:48 +0500

    Configuration for working with the `Books` table has been added to the database access layer

---
+++ BookStore.DataAccess/Configurations/BookConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStore.DataAccess.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<BookEntity>
+    {
+        public void Configure(EntityTypeBuilder<BookEntity> builder)
+        {
+            builder.HasKey(book => book.BookID);
+            builder.Property(book => book.BookTitle)
+                .IsRequired()                           
+                .HasMaxLength(Book.MAX_TITLE_LENGTH);   
+            builder.Property(book => book.BookDescription)
+                .IsRequired();                          
+            builder.Property(book => book.BookPrice)
+                .IsRequired();                          
+        }
+    }
+}
