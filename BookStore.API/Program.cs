6e6651d3b88d732ed68d15f36d9fc1e7dbb9931f
EBuravskiy <elric0sphinx@gmail.com>
2026-08-03 16:28:52 +0500

    An SQLite database has been connected to the application.

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,6 +1,16 @@
+using BookStore.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BookStoreDbContext>
+    (
+        options =>
+        {
+            options.UseSqlite(builder.Configuration.GetConnectionString(nameof(BookStoreDbContext)));
+        }
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
