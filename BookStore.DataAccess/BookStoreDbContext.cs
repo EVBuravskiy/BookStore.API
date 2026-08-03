9926d4e281b0a7339e3e63a495c9c7ddd0022950
EBuravskiy <elric0sphinx@gmail.com>
2026-08-03 12:59:52 +0500

    A database access layer has been added, containing an entity model and a context class.

---
+++ BookStore.DataAccess/BookStoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DataAccess
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public DbSet<BookEntity> Books { get; set; } = null!;
+
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options) 
+        { 
+        }
+    }
+}
---
+++ BookStore.DataAccess/Entities/BookEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.DataAccess.Entities
+{
+    public class BookEntity
+    {
+        public Guid BookID { get; set; }
+        public string BookTitle { get; set; } = string.Empty;
+        public string BookDescription { get; set; } = string.Empty;
+        public decimal BookPrice { get; set; }
+    }
+}
