e5a2c58241242fdaf036b83c2994510034b70544
EBuravskiy <elric0sphinx@gmail.com>
2026-08-04 09:56:55 +0500

    A service layer has been added, and the BookService and its interface have been implemented.

--- BookStore.BL/Services/BookService.cs
+++ BookStore.BL/Services/BookService.cs
@@ -1,6 +1,40 @@
-﻿namespace BookStore.BL.Services
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Repositories;
+
+namespace BookStore.BL.Services
 {
-    internal class BookService
+    public class BookService : IBookService
     {
+        private readonly IBooksRepository _booksRepository;
+
+        public BookService(IBooksRepository bookRepository)
+        {
+            _booksRepository = bookRepository;
+        }
+
+        public async Task<List<Book>> GetAllBooks()
+        {
+            return await _booksRepository.GetAll();
+        }
+
+        public async Task<Book> GetBook(Guid bookID)
+        {
+            return await _booksRepository.Get(bookID);
+        }
+
+        public async Task<Guid> CreateBook(Book book)
+        {
+            return await _booksRepository.Create(book);
+        }
+
+        public async Task<Guid> UpdateBook(Guid bookID, string title, string description, decimal price)
+        {
+            return await _booksRepository.Update(bookID, title, description, price);
+        }
+
+        public async Task<Guid> DeleteBook(Guid bookID)
+        {
+            return await _booksRepository.Delete(bookID);
+        }
     }
 }
---
+++ BookStore.Core/Abstractions/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Core.Models;
+
+namespace BookStore.BL.Services
+{
+    public interface IBookService
+    {
+        Task<Guid> CreateBook(Book book);
+        Task<Guid> DeleteBook(Guid bookID);
+        Task<List<Book>> GetAllBooks();
+        Task<Book> GetBook(Guid bookID);
+        Task<Guid> UpdateBook(Guid bookID, string title, string description, decimal price);
+    }
+}
